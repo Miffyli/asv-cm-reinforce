39552238bdf4d6e1e5de4b9f1fc8c2d68f33e35b
Anssi "Miffyli" Kanervisto <kaneran21@hotmail.com>
2020-02-06 01:13:01 +0200

    Fix running scripts

--- scripts/render_metrics_average_repetitions_paper.sh
+++ scripts/render_metrics_average_repetitions_paper.sh
@@ -5,15 +5,15 @@
 for pg_reward in "simple" "penalize" "reward" "tdcf"
 do
     ./scripts/render_metrics_average_paper.sh \
-        joint_rl_pg_${pg_reward}_epochs_50_repetition_1 \
-        joint_rl_pg_${pg_reward}_epochs_50_repetition_2 \
-        joint_rl_pg_${pg_reward}_epochs_50_repetition_3
+        joint_rl_pg_${pg_reward}_repetition_1 \
+        joint_rl_pg_${pg_reward}_repetition_2 \
+        joint_rl_pg_${pg_reward}_repetition_3
 done
 
 for ce_train in "ce_same" "ce_split"
 do
     ./scripts/render_metrics_average_paper.sh \
-        joint_rl_${ce_train}_epochs_50_repetition_1 \
-        joint_rl_${ce_train}_epochs_50_repetition_2 \
-        joint_rl_${ce_train}_epochs_50_repetition_3
+        joint_rl_${ce_train}_repetition_1 \
+        joint_rl_${ce_train}_repetition_2 \
+        joint_rl_${ce_train}_repetition_3
 done
